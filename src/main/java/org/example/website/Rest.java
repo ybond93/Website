b9bd3e4be6cf132dfcba19fb362a7c6be7622e04
ItsNotAlwaysTheSame <97375291+ItsNotAlwaysTheSame@users.noreply.github.com>
2024-02-21 16:13:54 +0100

    added Events and Reservations db and Rest files

---
+++ src/main/java/org/example/website/Rest.java
@@ -0,0 +1,9 @@
+package org.example.website;
+
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+@ApplicationPath("/api")
+public class Rest extends Application{
+
+}
