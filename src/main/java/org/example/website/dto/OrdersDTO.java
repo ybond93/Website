be0a132d0dbc8799814f3aa007ba7d396a630646
jakob <me@example.com>
2024-02-28 22:24:08 +0100

    +OrderDTO class

---
+++ src/main/java/org/example/website/dto/OrdersDTO.java
@@ -0,0 +1,41 @@
+package org.example.website.dto;
+
+import jakarta.persistence.*;
+import org.example.website.entities.MenuItemsEntity;
+import org.example.website.entities.TablesEntity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class OrdersDTO {
+
+    private int orderId;
+
+    private int tableNum;
+
+    private List<Integer> menuItems;
+
+    public int getTableNum() {
+        return tableNum;
+    }
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public List<Integer> getMenuItems() {
+        return menuItems;
+    }
+
+    public void setTableNum(int tableNum) {
+        this.tableNum = tableNum;
+    }
+
+    public void setMenuItems(List<Integer> menuItems) {
+        this.menuItems = menuItems;
+    }
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+}
