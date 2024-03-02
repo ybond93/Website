da020a20b3f44f9ced16074cfd98aca26a4ee37e
jakob <me@example.com>
2024-03-02 16:42:43 +0100

    added setter and getter for menuitemDTO

--- src/main/java/org/example/website/dto/MenuItemsDTO.java
+++ src/main/java/org/example/website/dto/MenuItemsDTO.java
@@ -8,5 +8,45 @@ public class MenuItemsDTO {
     private String category; // Assuming category is a string. If it's an entity, use CategoryDTO
 
     // plus constructors, getters, and setters
+
+    public int getMenuItemId() {
+        return menuItemId;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setMenuItemId(int menuItemId) {
+        this.menuItemId = menuItemId;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
 
