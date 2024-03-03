1ed6c17fd4d98590e175243220e4836e43ba7771
jakob <me@example.com>
2024-03-03 16:18:37 +0100

    fixed post

--- src/main/java/org/example/website/controllers/MenuItemsOrdersManager.java
+++ src/main/java/org/example/website/controllers/MenuItemsOrdersManager.java
@@ -2,15 +2,15 @@ package org.example.website.controllers;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
+import jakarta.transaction.Transactional;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.Response;
 import org.example.website.dto.MenuItemOrdersDTO;
 import org.example.website.entities.MenuItemOrdersEntity;
 import org.example.website.entities.MenuItemsEntity;
 import org.example.website.mapper.MenuItemOrderMapper;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 @Path("/menuitemorders")
@@ -27,6 +27,20 @@ public class MenuItemsOrdersManager {
                 .collect(Collectors.toList());
         return Response.ok(menuItemOrdersDTOs).build();
 
+    }
+    @POST
+    @Consumes("application/json")
+    @Transactional
+    public  Response creat(MenuItemOrdersDTO menuItemOrdersDTO){
+        MenuItemOrdersEntity menuItemOrdersEntity = new MenuItemOrdersEntity();
+        menuItemOrdersEntity.setMenuItemId(menuItemOrdersDTO.getItemId());
+        menuItemOrdersEntity.setOrderId(menuItemOrdersDTO.getOrderId());
+        menuItemOrdersEntity.setAmount(menuItemOrdersDTO.getAmount());
+
+
+        em.persist(menuItemOrdersEntity);
+        em.flush();
+        return Response.created(URI.create("/menuitemorders")).build();
     }
 
 }
